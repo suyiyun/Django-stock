83d028df247e712ed53ec5652449ae6042d909bd
suyiyun <su_yiyun@outlook.com>
2021-05-09 20:42:29 -0400

    add predict.html

--- quotes/urls.py
+++ quotes/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('add_stock.html', views.add_stock, name="add_stock"),
     path('delete/<stock_id>', views.delete, name="delete"),
     path('delete_stock.html', views.delete_stock, name="delete_stock"),
+    path("predict", views.predict, name='predict'),
 ]
